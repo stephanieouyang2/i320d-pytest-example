26d80427f84c6b3dfb6664addbc3b1737558cdc6
stephanieouyang2 <stephanieouyang@utexas.edu>
2025-03-26 16:38:32 -0500

    Update my_code.py

--- my_code.py
+++ my_code.py
@@ -17,5 +17,5 @@ def test_fix_phone_num():
   assert fix_phone_num("3216543333") == '(321) 654 3333'
 
 def test_fix_phone_num_with_extra_chars():
-    assert fix_phone_num("555-442-98761") == "555-442-9876"
-    assert fix_phone_num("(321) 654 3333") == "321-654-3333"
+    assert fix_phone_num("555-442-98761") == "(555) 442 9876"
+    assert fix_phone_num("(321) 654 3333") == "(321) 654 3333"

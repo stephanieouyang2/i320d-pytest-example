de7aef308337161bb0df971217c5b192d53b7255
stephanieouyang2 <stephanieouyang@utexas.edu>
2025-03-27 13:45:53 -0500

    Update my_code.py
    Test should now document why the tests are failing

--- my_code.py
+++ my_code.py
@@ -19,3 +19,9 @@ def test_fix_phone_num():
 def test_fix_phone_num_with_extra_chars():
     assert fix_phone_num("555-442-98761") == "(555) 442 9876"
     assert fix_phone_num("(321) 654 3333") == "(321) 654 3333"
+
+def phone_num_value_error():
+  with pytest.raises(ValueError):
+    fix_phone_num("abc-def-ghij")
+
+    
